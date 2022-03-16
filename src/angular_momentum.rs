2860bbb417147b2271281cbbfc9dcbbb4f1c276b
Joscha Hoche <37776098+hochej@users.noreply.github.com>
2022-03-16 13:58:32 +0100

    add From<u8> for AngularMomentum

--- src/angular_momentum.rs
+++ src/angular_momentum.rs
@@ -33,6 +33,26 @@ impl Hash for AngularMomentum {
     }
 }
 
+impl From<u8> for AngularMomentum {
+    /// Panics for num > 10, as the angular momentum is only implemted up to M=10.
+    fn from(num: u8) -> Self {
+        match num {
+            0 => Self::S,
+            1 => Self::P,
+            2 => Self::D,
+            3 => Self::F,
+            4 => Self::G,
+            5 => Self::H,
+            6 => Self::I,
+            7 => Self::J,
+            8 => Self::K,
+            9 => Self::L,
+            10 => Self::M,
+            a => panic!("{} is not a valid angular momentum, only numbers between 0 and 10 are supported", a),
+        }
+    }
+}
+
 impl fmt::Display for AngularMomentum {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let symbol: &str = match self {
